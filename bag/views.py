f354b78ee7d5be0ae65dae27c1c9bcd219056dec
Kitti Kovacs <bambula86@gmail.com>
2021-01-23 14:27:56 +0000

    Updated the shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -22,5 +22,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
